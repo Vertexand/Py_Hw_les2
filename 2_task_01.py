3406fd65cbbd562b85b4456661c1ad2ecafab2c5
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-10-01 14:52:17 +0300

    add task1

--- 2_task_01.py
+++ 2_task_01.py
@@ -1,2 +1,20 @@
 # 1. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 # Пример:  6782 -> 23, 0,67 ->13,  0,56 -> 11
+# num_1 = input ("введите ")
+# print(sum([int(i) for i in num_1 if i.isdigit()]))
+
+
+num = float(input())# вар 2 Без работы с методами строк.
+sum_digits = 0
+
+power = len(str(num)) - 2
+num *= 10 ** power
+
+while num:
+    sum_digits += num % 10
+    num //= 10
+
+print(int(sum_digits))
+
+# проверка на числа с минусом :
+# while not (num.isdigit() or num[0] == '-' and  num[1:].isdigit()):
