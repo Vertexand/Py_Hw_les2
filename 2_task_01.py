fd5eebae5db05a408213aeae443a3590da75936f
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-10-01 15:59:55 +0300

    add +варианты

--- 2_task_01.py
+++ 2_task_01.py
@@ -4,17 +4,29 @@
 # print(sum([int(i) for i in num_1 if i.isdigit()]))
 
 
-num = float(input())# вар 2 Без работы с методами строк.
-sum_digits = 0
+# num = float(input())# вар 2 Без работы с методами строк.
+# sum_digits = 0
 
-power = len(str(num)) - 2
-num *= 10 ** power
+# power = len(str(num)) - 2
+# num *= 10 ** power
 
-while num:
-    sum_digits += num % 10
-    num //= 10
+# while num:
+#     sum_digits += num % 10
+#     num //= 10
 
-print(int(sum_digits))
+# print(int(sum_digits))
+
+num = float(input('Enter number: '))# вар 3
+
+num_int = int(str(num).replace('.', ''))
+sum = 0
+
+while num_int > 0:
+    cut_digit = num_int % 10
+    sum += cut_digit
+    num_int //= 10
+
+print(sum)
 
 # проверка на числа с минусом :
 # while not (num.isdigit() or num[0] == '-' and  num[1:].isdigit()):
